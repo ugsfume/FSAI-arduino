a7391d2cbfc4d9032150e465d94f10ad35bb64da
Chunchun-Zheng <158860179+Chunchun-Zheng@users.noreply.github.com>
2024-05-01 16:21:14 +0100

    Update servo_control.ino
    insert a limit for servo rotating angle

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -3,18 +3,21 @@
 Servo myservo;  // create servo object to control a servo
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600); // initialize serial communication at 9600 bits per second:
 }
 
 void loop() {
   int sensorValue = analogRead(A0);              // read the value from the analog input
   int angle = map(sensorValue, 0, 1023, 0, 180); // map it to a range of 0 to 180
+  if(angle>51){ // The range of usable servo angle is 0(left most) to 51(right most)
+    angle=51;
+  }
   
   myservo.write(angle); // sets the servo position according to the scaled value
   
   // Calculate the percentage of the sensor value
-  int percentage = map(sensorValue, 0, 1023, 0, 100);
+  int percentage = map(sensorValue, 0, 1023, 0, 180);
   
   // Print the percentage to the Serial Monitor
   Serial.print("Analog Reading in Percentage: ");
