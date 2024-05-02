06aaf66c37fce269e04c2cd2bf7fe9ccdf3c704e
Chunchun-Zheng <158860179+Chunchun-Zheng@users.noreply.github.com>
2024-05-02 16:59:14 +0100

    Update dc_motor_PWM.ino

--- dc_motor/dc_motor_PWM.ino
+++ dc_motor/dc_motor_PWM.ino
@@ -10,13 +10,16 @@ void setup() {
 
 void loop() {
   int sensorValue = analogRead(A0);              // read the value from the analog input
-  int ratio = map(sensorValue, 0, 460, 0, 100); // map it to a range of 0 to 180
+  Serial.println(sensorValue);
 
+  int ratio = map(sensorValue, 0, 1023, 0, 180); // map it to a range of 0 to 180
+  // AnalogRead = 513 is when the car is moving at controllable speed
   myservo.write(ratio); // sets the servo position according to the scaled value
   
+  int percentage = map(ratio, 0, 180, 0, 100);
   // Print the percentage to the Serial Monitor
-  Serial.print("PMW ratio: ");
-  Serial.println(ratio);
+  Serial.print("PMW percentage: ");
+  Serial.println(percentage);
   
-  delay(15); // waits for the servo to get there
+  delay(500); // waits for the servo to get there
 }
