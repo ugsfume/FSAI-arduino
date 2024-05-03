c398f1852e6178e274ef036cfc89bbbe6878d1a1
Chunchun-Zheng <158860179+Chunchun-Zheng@users.noreply.github.com>
2024-05-03 09:46:09 +0100

    Update motor.ino

--- dc_motor/motor.ino
+++ dc_motor/motor.ino
@@ -8,10 +8,12 @@ void loop() {
   int sensorValue = analogRead(A0);              // read the value from the analog input
   Serial.println(sensorValue);
 
-  int pwmRatio = map(sensorValue, 0, 1023, 0, 255); // map it to a range of 0 to 180
-
+  int pwmRatio = map(sensorValue, 0, 1023, 0, 255); // map it to a range of 0 to 255
   // Write the PWM signal to the motor pin
   analogWrite(10, pwmRatio);
-  
+   int percentage = map(pwmRatio, 0, 255, 0, 100);
+  // Print the percentage to the Serial Monitor
+  Serial.print("PMW percentage: ");
+  Serial.println(percentage);
   delay(500); // waits for the servo to get there
 }
